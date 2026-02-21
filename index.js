2f644062d734313c3b3ac91565bd4edf2b47a59d
dhina <dhinakarandv7@gmail.com>
2026-02-21 22:38:18 +0530

    updated js

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 console.log("hello welcome to the git session")
-<<<<<<< HEAD
 for(var i=0;i<8;i++){
     console.log("hello")
-=======
+}
 for( var i=0;i<5;i++){
     console.log(i)
->>>>>>> main
 }
